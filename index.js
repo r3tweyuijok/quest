399a74858f231800bac6b811f2c3200b73753d46
r3tweyuijok <hsjcovercraft12@hotmail.com>
2024-05-13 11:31:57 +0700

    test project

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
-function isValidBST(root) {
-  return isValid(root, null, null);
-  function isValid(node, min, max) {
-    if (!node) return true;
-    if ((min !== null && node.val <= min) || (max !== null && node.val >= max))
-      return false;
-    return (
-      isValid(node.left, min, node.val) && isValid(node.right, node.val, max)
-    );
-  }
-}
+const groupBy = (arr, func) =>
+  arr
+    .map(typeof func === "function" ? func : (val) => val[func])
+    .reduce((acc, val, i) => {
+      acc[val] = (acc[val] || []).concat(arr[i]);
+      return acc;
+    }, {});
